7221ad97ee5a6946b7da33ca27287794ee31d418
peterchen999 <peter.chen999@gmail.com>
2022-12-04 22:04:08 +0800

    new - new ApiResult module for checking api result
    - new file ApiResult.rb
    - modified new_user_nickname.rb to call ApiResult correctly

--- app/application/requests/new_user_nickname.rb
+++ app/application/requests/new_user_nickname.rb
@@ -15,7 +15,7 @@ module TravellingSuggestions
       rescue StandardError
         Failure(
           Reponse::ApiResult.new(
-            status: :bad_request
+            status: :forbidden
             message: 'Invalid nickname'
           )
         )
