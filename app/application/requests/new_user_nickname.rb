c64b4f576058aad507ad3a2ff2872d51761b266f
peterchen999 <peter.chen999@gmail.com>
2022-12-04 21:40:36 +0800

    new - added request object for user nickname
    - new file new_user_nickname.rb
    - use same rule as former form object NewUserNickname

---
+++ app/application/requests/new_user_nickname.rb
@@ -0,0 +1,29 @@
+module TravellingSuggestions
+  module Request
+    class EncodedNewUserNickname
+      include Dry::Monads::Result::Mixin
+      NICKNAME_REGEX = %r{/^\w+$/}
+
+      def initialize(params)
+        @params = params
+      end
+
+      def call
+        Success(
+          JSON.parse(rule(@params['nickname']))
+        )
+      rescue StandardError
+        Failure(
+          Reponse::ApiResult.new(
+            status: :bad_request
+            message: 'Invalid nickname'
+          )
+        )
+      end
+
+      self.rule(param) do
+        NICKNAME_REGEX.match?(param) ? param : raise StandardError.new
+      end
+    end
+  end
+end
