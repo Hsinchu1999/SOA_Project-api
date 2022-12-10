a8ec97e1f49ff31b893f3ddcf85fdb1bea8440e0
peterchen999 <peter.chen999@gmail.com>
2022-12-10 19:37:11 +0800

    new - new Service::MBTIQuestion object for mbti questions
    - new file list_mbti_question.rb for new Service object
    -

---
+++ app/application/services/list_mbti_question.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'dry/monads'
+
+module TravellingSuggestions
+  module Service
+    # A Service object to validate Entity::Weather object from db
+    class ListMBTIQuestion
+      include Dry::Monads::Result::Mixin
+
+      def call(question_id)
+        mbti_question = Repository::MBTIQuestions.find_id(question_id)
+        Success(
+          Response::ApiResult.new(
+            status: :ok,
+            message: mbti_question
+          )
+        )
+      rescue StandardError
+        Failure(
+          Response::ApiResult.new(
+            status: :not_found,
+            message: 'Could not fetch mbti question from database'
+          )
+        )
+      end
+    end
+  end
+end
