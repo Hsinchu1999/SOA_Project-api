11b29ce7c5aaa21a6be8d30e3d332f212521fc2f
peterchen999 <peter.chen999@gmail.com>
2022-12-16 23:17:55 +0800

    new - new Request object for ensuring mbti question set input validity
    - new file requests/list_mbti_question_set.rb
    - validate mbti questionset parameter 'set_size" correctness
    - must be positive integer
    - must > 0 & <= 11

---
+++ app/application/requests/list_mbti_questin_set.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'dry/monads'
+require 'json'
+
+module TravellingSuggestions
+  module Request
+    # A Request object for calculating mbti score
+    class EncodedMBTIQuestionSet
+      include Dry::Monads::Result::Mixin
+
+      def initialize(params)
+        @params = params
+      end
+
+      def call
+        Success(
+          rule
+        )
+      rescue StandardError
+        Failure(
+          Response::ApiResult.new(
+            status: :bad_request,
+            message: 'Incorrect mbti question size input'
+          )
+        )
+      end
+
+      def rule
+        set_size = @params[:set_size]
+        raise StandardError unless set_size !~ /\D/
+        raise StandardError unless set_size.to_i <= 11
+        raise StandardError unless set_size.to_i > 0
+
+        set_size
+      end
+    end
+  end
+end
