5fd5c44d4b1407a4bf721f82e25ad1ca43b18dc3
peterchen999 <peter.chen999@gmail.com>
2022-12-16 21:21:06 +0800

    chores - removed unused comments in helpers/spec_helper.rb

--- spec/helpers/spec_helper.rb
+++ spec/helpers/spec_helper.rb
@@ -15,10 +15,6 @@ require 'webmock'
 require_relative '../../require_app'
 require_app
 
-# CONFIG = YAML.safe_load(File.read('config/secrets.yml'))
-# CWB_TOKEN = CONFIG['test']['CWB_TOKEN']
-# YML_FILE = YAML.safe_load(File.read('spec/fixtures/cwb_results.yml'))
-
 MBTI_QUESTION_ID = Array(1..10)
 VALID_NICKNAMES = ['peter', 'Peter', 'Peter_', '_PETER', '_', 'PETERCHEN', 'peter_chen', 'peterchen999']
 INVALID_NICKNAMES = [' ', '.', '', './']
