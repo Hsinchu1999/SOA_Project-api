59557abc31d88621d046e02242354d54e0d3e3a3
peterchen999 <peter.chen999@gmail.com>
2022-12-17 15:57:02 +0800

    refactor - refactored tests, test passed

--- spec/helpers/spec_helper.rb
+++ spec/helpers/spec_helper.rb
@@ -16,15 +16,15 @@ require_relative '../../require_app'
 require_app
 
 MBTI_QUESTION_ID = Array(1..10)
-VALID_NICKNAMES = ['peter', 'Peter', 'Peter_', '_PETER', '_', 'PETERCHEN', 'peter_chen', 'peterchen999']
-INVALID_NICKNAMES = [' ', '.', '', './']
+VALID_NICKNAMES = %w[peter Peter Peter_ _PETER _ PETERCHEN peter_chen peterchen999].freeze
+INVALID_NICKNAMES = [' ', '.', '', './'].freeze
 
-VALID_MBTI_QUESTION_PAIR = ['1=A&2=B&3=A&4=B', '9=B&10=A&11=A&14=A']
-CORRECT_MBTI_QUESTION_RESULT = ['ENTJ', 'ISFJ']
-INVALID_MBTI_QUESTION_PAIR = ['0=A&1=A&2=A&3=A', '99=A&1=A&3=A&4=A']
+VALID_MBTI_QUESTION_PAIR = ['1=A&2=B&3=A&4=B', '9=B&10=A&11=A&14=A'].freeze
+CORRECT_MBTI_QUESTION_RESULT = %w[ENTJ ISFJ].freeze
+INVALID_MBTI_QUESTION_PAIR = ['0=A&1=A&2=A&3=A', '99=A&1=A&3=A&4=A'].freeze
 
 VALID_MBTI_QUESTION_SET_SIZE = (1..11).to_a
-INVALID_MBTI_QUESTION_SET_SIZE = ['a', '-1', '99', '12', '0']
+INVALID_MBTI_QUESTION_SET_SIZE = ['a', '-1', '99', '12', '0'].freeze
 
 CASSETTE_FOLDER = 'spec/fixtures/cassettes'
 CASSETTE_FILE = 'web_api'
