a9e6aafb57741d45abba597a63c76cd1fde16844
peterchen999 <peter.chen999@gmail.com>
2022-12-20 22:42:41 +0800

    bugfix - fixed incorrect vcr helper

--- spec/helpers/vcr_helper.rb
+++ spec/helpers/vcr_helper.rb
@@ -12,14 +12,14 @@ module VcrHelper
     VCR.configure do |c|
       c.cassette_library_dir = CASSETTE_FOLDER
       c.hook_into :webmock
+      c.ignore_hosts 'sqs.us-east-1.amazonaws.com'
+      c.ignore_hosts 'sqs. ap-northeast-1.amazonaws.com'
     end
   end
 
   def self.configure_vcr_for_cwb
     VCR.configure do |c|
       c.filter_sensitive_data('<CWB_TOKEN>') { CWB_TOKEN }
-      vcr_config.ignore_hosts 'sqs.us-east-1.amazonaws.com'
-      vcr_config.ignore_hosts 'sqs. ap-northeast-1.amazonaws.com'
     end
     VCR.insert_cassette(CASSETTE_FILE, record: :new_episodes, match_requests_on: %i[method uri headers])
   end
