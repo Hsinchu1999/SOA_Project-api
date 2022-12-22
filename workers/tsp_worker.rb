b9db2713081971a0f7e700139b0623e7777cd73a
peterchen999 <peter.chen999@gmail.com>
2022-12-22 18:01:00 +0800

    bugfix - fixed some bugs in worker, PoC completed
    - modified shoryuken.yml & tsp_worker.rb
    - fixed typos in tsp_worker.rb
    - added Aws::SQS object into tsp_worker
    - added missing indent in shoryuken.yml

--- workers/tsp_worker.rb
+++ workers/tsp_worker.rb
@@ -4,19 +4,28 @@ require_app
 require 'figaro'
 require 'shoryuken'
 
+
+# TSP worker class to solve TSP problems in background
 class TSPWorker
   Figaro.application = Figaro::Application.new(
     environment: ENV['RACK_ENV'] || 'development',
-    path: File.expand('config/secrets.yml')
+    path: File.expand_path('config/secrets.yml')
   )
   Figaro.load
   def self.config = Figaro.env
+
+    Shoryuken.sqs_client = Aws::SQS::Client.new(
+      access_key_id: config.AWS_ACCESS_KEY_ID,
+      secret_access_key: config.AWS_SECRET_ACCESS_KEY,
+      region: config.AWS_REGION
+    )
 
   include Shoryuken::Worker
   shoryuken_options queue: config.TSP_QUEUE_URL, auto_delete: true
 
   def perform(_sqs_msg, request)
-    puts 'to be completed'
+    puts 'in TSPWorker.perform'
+    puts request
   rescue
     puts 'something went wrong'
   end
