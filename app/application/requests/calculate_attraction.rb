82eb97064ad1e49a2f01729078e4541fee424de4
peterchen999 <peter.chen999@gmail.com>
2022-12-31 14:09:30 +0800

    new - new Request object to check attraction input
    - new file calculate_attraction.rb
    - checks if each attraction id is a number string
    - checks if each attraction is either like or dislike

---
+++ app/application/requests/calculate_attraction.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'dry/monads'
+require 'json'
+
+module TravellingSuggestions
+  module Request
+    # A Request object for calculating mbti score
+    class EncodedAttraction
+      include Dry::Monads::Result::Mixin
+
+      def initialize(params)
+        @params = params
+      end
+
+      def call
+        Success(
+          rule
+        )
+      rescue StandardError
+        Failure(
+          Response::ApiResult.new(
+            status: :bad_request,
+            message: 'Incorrect mbti question id'
+          )
+        )
+      end
+
+      def rule
+        @params.each do |key, value|
+          raise StandardError unless key !~ /\D/
+          raise StandardError unless ['like', 'dislike'].include? value
+        end
+
+        @params
+      end
+    end
+  end
+end
