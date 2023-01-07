b9294db6f89a3f824dc7c0a9e865dff128757a69
peterchen999 <peter.chen999@gmail.com>
2023-01-07 22:29:49 +0800

    new - new Service object to list attraction mbti rating
    - new file list_attraction_mbti_rating.rb
    - calls Repository::AttractionMbtiRatings to find a single attraction mbti rating

---
+++ app/application/services/list_attraction_mbti_rating.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'dry/monads'
+
+module TravellingSuggestions
+  module Service
+    # A Service object to validate result from db
+    class ListAttractionMbtiRating
+      include Dry::Monads::Result::Mixin
+
+      def call(input)
+        # puts "In Service::ListAttractionMbtiRating.list_attraction_mbti_rating"
+        # puts "input=#{input}"
+        
+
+        attraction_id = input[:attraction_id].to_i
+        # puts "attraction_id=#{attraction_id}"
+
+        attraction_mbti_rating = Repository::ForAttraction
+            .klass(Entity::AttractionMbtiRating)
+            .find_attraction_id(attraction_id)
+        
+        Success(
+            Response::ApiResult.new(
+              status: :ok,
+              message: attraction_mbti_rating
+            )
+          )
+      rescue StandardError
+        Failure(
+          Response::ApiResult.new(
+            status: :not_found,
+            message: 'Attraction Mbti rating not found'
+          )
+        )
+      end
+    end
+  end
+end
