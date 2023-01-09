ba46492b42ac0f724c81521376581214cf880e4f
peterchen999 <peter.chen999@gmail.com>
2023-01-09 22:29:31 +0800

    modify - changed cassette folder & file variable name

--- spec/helpers/vcr_helper.rb
+++ spec/helpers/vcr_helper.rb
@@ -5,12 +5,10 @@ require 'webmock'
 
 # Setting up VCR
 module VcrHelper
-  CASSETTE_FOLDER = 'spec/fixtures/cassettes'
-  CASSETTE_FILE = 'cwb_api'
 
   def self.setup_vcr
     VCR.configure do |c|
-      c.cassette_library_dir = CASSETTE_FOLDER
+      c.cassette_library_dir = CASSETTE_FOLDER_CWB
       c.hook_into :webmock
       c.ignore_hosts 'sqs.us-east-1.amazonaws.com'
       c.ignore_hosts 'sqs. ap-northeast-1.amazonaws.com'
@@ -21,7 +19,7 @@ module VcrHelper
     VCR.configure do |c|
       c.filter_sensitive_data('<CWB_TOKEN>') { CWB_TOKEN }
     end
-    VCR.insert_cassette(CASSETTE_FILE, record: :new_episodes, match_requests_on: %i[method uri headers])
+    VCR.insert_cassette(CASSETTE_FILE_CWB, record: :new_episodes, match_requests_on: %i[method uri headers])
   end
 
   def self.eject_vcr
